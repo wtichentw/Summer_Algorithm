7cdd46885def78badf1edb7531bd8ab63da37fc4
z0012420014 <z0012420014@gmail.com>
2018-06-30 21:11:44 -0400

    Add files via upload

---
+++ 119.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    class Solution {
+        public List<Integer> getRow(int rowIndex) {
+            List<Integer> ans = new ArrayList<Integer>();
+
+            for (int i = 0; i <= rowIndex; i++) {
+                ans.add(0, 1);
+                for (int j = 1; j < ans.size() - 1; j++) {
+                    ans.set(j, ans.get(j) + ans.get(j + 1));
+                }
+            }
+
+            return ans;
+        }
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
