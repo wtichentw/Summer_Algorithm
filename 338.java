991de335b7b2502b759c5082d87c037512254292
z0012420014 <z0012420014@gmail.com>
2018-07-01 20:50:06 -0400

    Counting Bits (medium)
    Counting Bits (medium)

---
+++ 338.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    class Solution {
+        public int[] countBits(int num) {
+            int[] dp = new int[num + 1];
+            dp[0] = 0;
+            for (int i = 0; i <= num; i++) {
+                dp[i] = dp[i >> 1] + (i & 1);
+            }
+            return dp;
+        }
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
