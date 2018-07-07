0358ea860e03a405bbe7c837a96bdabd5df1ec64
z0012420014 <z0012420014@gmail.com>
2018-07-06 22:13:21 -0400

    Search Insert Position ( easy )
    Search Insert Position ( easy )

---
+++ 035.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Main {
+
+    class Solution {
+        public int searchInsert(int[] nums, int target) {
+            int start = 0, end = nums.length - 1;
+
+            while (start + 1 < end) {
+                int mid = (start + end) / 2;
+                if (nums[mid] == target) {
+                    return mid;
+                } else if (nums[mid] > target) {
+                    end = mid;
+                } else {
+                    start = mid;
+                }
+            }
+
+            //  target__start__target__end__target
+            if (target <= nums[start]) {
+                return start;
+            } else if (target <= nums[end]) {
+                return end;
+            } else {
+                return end + 1;
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
