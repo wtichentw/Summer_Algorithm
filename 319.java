5b1e5ec466b9fef3bbd0bb2a1bd7f21c5a023b80
z0012420014 <z0012420014@gmail.com>
2018-07-23 22:00:03 -0400

    Bulb Switcher (medium)
    Bulb Switcher (medium)

---
+++ 319.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    class Solution {
+        public int bulbSwitch(int n) {
+            //all factors in pairs
+            return (int) Math.sqrt(n);
+        }
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
