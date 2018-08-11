cd95ff60c2459b88992b0e745b700a6f4628b46d
z0012420014 <z0012420014@gmail.com>
2018-08-10 22:21:20 -0400

    Container With Most Water (medium)
    Container With Most Water (medium)

---
+++ 011.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int area = 0;
+        int left = 0, right = height.length - 1;
+        
+        while (left < right) {
+            int h = Math.min(height[left], height[right]);
+            area = Math.max(area, h * (right - left));
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        
+        return area;
+    }
+}
